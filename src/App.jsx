78030b85d108ec628d28f950d7d8844a8109e896
Nama' Salameh <nama.salameh@gmail.com>
2023-09-23 13:02:23 +0300

    Modified members .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import FilterMembers from "./components/FilterMembers";
-import styles from "./App.module.css"
+import MembersList from "./components/MembersList";
+import styles from "./App.module.css";
 export default function App() {
   return (
-    <div className={styles.appContainer} >
-    <p> <b>Add members to Front-end development team </b> </p>
-      <FilterMembers/>
+    <div className={styles.appContainer}>
+      <p>
+        <b>Add members to Front-end development team </b>
+      </p>
+      <MembersList />
     </div>
   );
 }
---
+++ src/components/MembersList.jsx
@@ -0,0 +1,49 @@
+import Members from "./Members";
+import SeachBar from "./SearchBar";
+import SubmitButton from "./SubmitButtons";
+import styles from "./../App.module.css";
+
+export default function MembersList() {
+  const members = [
+    {
+      name: "Carolien Bloeme",
+      isMember: false,
+    },
+    {
+      name: "Sun Jun",
+      isMember: true,
+    },
+    {
+      name: "Song Bao",
+      isMember: false,
+    },
+    {
+      name: "Olivia Arribas",
+      isMember: true,
+    },
+    {
+      name: "Bonginkosi Mdladlana",
+      isMember: false,
+    },
+    {
+      name: "Arina Belomestnykh",
+      isMember: true,
+    },
+    {
+      name: "Jacqueline Likoki",
+      isMember: true,
+    },
+  ];
+  return (
+    <div className={styles.membersContainer}>
+      <div className={styles.membersList}>
+        <SeachBar />
+
+        {members.map((member, index) => (
+          <Members key={index} memberInfo={member} />
+        ))}
+      </div>
+      <SubmitButton />
+    </div>
+  );
+}
